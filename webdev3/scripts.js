0da97febe1c178b788ffcbdb6a2e3868f90cb542
Witek ten Hove <wfth69@gmail.com>
2020-09-22 22:40:50 +0200

    Update scripts.js

--- webdev3/scripts.js
+++ webdev3/scripts.js
@@ -1,9 +1,16 @@
 // Initialize firestore
-firebase.initializeApp({
-    apiKey: 'AIzaSyCUk6-YcDJrcA3Vs6Z2mmGfDTNbD7xCyHE',
-    authDomain: 'smartconnection-9d342.firebaseapp.com',
-    projectId: 'smartconnection-9d342'
-  });
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCUk6-YcDJrcA3Vs6Z2mmGfDTNbD7xCyHE",
+    authDomain: "smartconnection-9d342.firebaseapp.com",
+    databaseURL: "https://smartconnection-9d342.firebaseio.com",
+    projectId: "smartconnection-9d342",
+    storageBucket: "smartconnection-9d342.appspot.com",
+    messagingSenderId: "467477051637",
+    appId: "1:467477051637:web:79703492fcd8c86889adbc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
   
 var db = firebase.firestore();
 
