5ea74f55a68666fc5caa3517e557ca8a20807cbf
witusj <wfth69@gmail.com>
2020-09-26 08:53:54 +0200

    Minor

--- webdev3/scripts.js
+++ webdev3/scripts.js
@@ -1,17 +1,17 @@
 // Initialize firestore
 // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCUk6-YcDJrcA3Vs6Z2mmGfDTNbD7xCyHE",
-    authDomain: "smartconnection-9d342.firebaseapp.com",
-    databaseURL: "https://smartconnection-9d342.firebaseio.com",
-    projectId: "smartconnection-9d342",
-    storageBucket: "smartconnection-9d342.appspot.com",
-    messagingSenderId: "467477051637",
-    appId: "1:467477051637:web:79703492fcd8c86889adbc"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
+var firebaseConfig = {
+apiKey: "AIzaSyCUk6-YcDJrcA3Vs6Z2mmGfDTNbD7xCyHE",
+authDomain: "smartconnection-9d342.firebaseapp.com",
+databaseURL: "https://smartconnection-9d342.firebaseio.com",
+projectId: "smartconnection-9d342",
+storageBucket: "smartconnection-9d342.appspot.com",
+messagingSenderId: "467477051637",
+appId: "1:467477051637:web:79703492fcd8c86889adbc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 var db = firebase.firestore();
 
 function runFunction() {
@@ -21,7 +21,7 @@ function runFunction() {
 }
 
 function writeFunction(naam, bericht) {
-    // Add a new document in collection "cities"
+    // Add a new document in collection "berichten"
     db.collection("berichten").add({
         naam: naam,
         bericht: bericht
